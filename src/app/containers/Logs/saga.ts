077fba1e727bf4ce8b1b699cb94d268ebc266717
dzg6 <mdr.photoshop@gmail.com>
2021-02-04 13:04:57 -0800

    api update

--- src/app/containers/Logs/saga.ts
+++ src/app/containers/Logs/saga.ts
@@ -3,14 +3,24 @@ import { logsActions } from './slice';
 import Amplify from '@aws-amplify/core';
 import { DataStore } from '@aws-amplify/datastore';
 import { Workout } from 'models';
+import { API } from 'aws-amplify';
+import * as queries from 'graphql/queries';
 
 import awsconfig from 'aws-exports';
 Amplify.configure(awsconfig);
 
 export function* doSomething() {
   try {
-    const workouts = yield DataStore.query(Workout);
-    yield put(logsActions.Playerlogs(workouts));
+    
+
+
+// Simple query
+const getWorkOuts = yield API.graphql({ query: queries.listWorkouts });
+  //const workouts = yield DataStore.query(Workout);
+  console.log(getWorkOuts.data.listWorkouts.items);
+    
+    
+    yield put(logsActions.Playerlogs(getWorkOuts.data.listWorkouts.items));
   } catch (error) {
     console.log('error saving post', error);
   }
--- src/app/containers/Tracker/saga.ts
+++ src/app/containers/Tracker/saga.ts
@@ -8,6 +8,11 @@ import { trackerActions } from './slice';
 
 
 import Amplify from "@aws-amplify/core";
+import { API } from "aws-amplify";
+
+import * as mutations from 'graphql/mutations';
+
+
 import { DataStore } from '@aws-amplify/datastore';
 import { Workout } from 'models';
 
@@ -15,29 +20,42 @@ import awsconfig from "aws-exports";
 Amplify.configure(awsconfig);
 
 export function* doSomething() {
-
-
-
-
+  
   const settings: { player; exercise; weight; resistanceBand } = yield select(
     selectSettings,
   );
 
   const time: { time } = yield select(selectStopWatch);
 
+  const tempWorkPut =      {   
+  date: new Date().toLocaleString().split(',')[0],
+  player: settings.player,
+  exercise: settings.exercise,
+  weight: settings.weight,
+  resistance: settings.resistanceBand.toString(),
+  time: time.time.toString(),
+  };
+
+  // const newTodo = await API.graphql({
+  //   query: mutations.createWorkout,
+  //    variables: {input: tempWorkPut}});
 
 
   try {
-    yield DataStore.save(
-      new Workout({
-        date: new Date().toLocaleString().split(',')[0],
-        player: settings.player,
-        exercise: settings.exercise,
-        weight: settings.weight,
-        resistance: settings.resistanceBand.toString(),
-        time: time.time.toString(),
-      }),
-    );
+
+    // yield DataStore.save(
+    //   new Workout({
+    //     date: new Date().toLocaleString().split(',')[0],
+    //     player: settings.player,
+    //     exercise: settings.exercise,
+    //     weight: settings.weight,
+    //     resistance: settings.resistanceBand.toString(),
+    //     time: time.time.toString(),
+    //   }),
+    // );
+    yield API.graphql({
+      query: mutations.createWorkout,
+       variables: {input: tempWorkPut}});
 
   } catch (error) {
     console.log('error saving post', error);
